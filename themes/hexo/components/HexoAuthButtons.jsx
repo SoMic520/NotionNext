4b6464b0955fea1f657e20da35aa9997145c0354
SoMic520 <jhzhou520@gmail.com>
2025-09-05 13:14:08 +0800

    Update HexoAuthButtons.jsx

--- themes/hexo/components/HexoAuthButtons.jsx
+++ themes/hexo/components/HexoAuthButtons.jsx
@@ -1,19 +1,26 @@
-'use client'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+ 'use client'
+ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+ import { useGlobal } from '@/lib/global'
 
-export default function HexoAuthButtons() {
-  return (
-    <div className="flex items-center">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button className="h-9 px-3 text-sm rounded-md border hover:bg-black/5">
-            登录
-          </button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton afterSignOutUrl="/" />
-      </SignedIn>
-    </div>
-  )
-}
+ export 默认 function HexoAuthButtons() {
+   const { locale } = useGlobal()
++  const tSignIn = locale?.SIGN_IN || 'Sign In'
+
+   return (
+     <div className="flex items-center">
+       <SignedOut>
+         <SignInButton mode="modal">
+-          <button className="h-9 px-3 text-sm rounded-md border hover:bg-black/5">
+-            {locale?.LOGIN || 'Sign In'}
+-          </button>
++          <button className="h-9 px-3 text-sm rounded-md border hover:bg-black/5">
++            {tSignIn}
++          </button>
+         </SignInButton>
+       </SignedOut>
+       <SignedIn>
+         <UserButton afterSignOutUrl="/" userProfileMode="navigation" />
+       </SignedIn>
+     </div>
+   )
+ }
