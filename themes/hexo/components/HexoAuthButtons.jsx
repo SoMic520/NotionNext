d5efa8da86fae1d3f5b96663754c10ccf0931fe1
SoMic520 <jhzhou520@gmail.com>
2025-09-05 08:37:36 +0800

    Update HexoAuthButtons.jsx
    修改登录按钮

--- themes/hexo/components/HexoAuthButtons.jsx
+++ themes/hexo/components/HexoAuthButtons.jsx
@@ -3,10 +3,12 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 export default function HexoAuthButtons() {
   return (
-    <div className="w-full flex justify-center md:justify-start md:w-auto mt-4 md:mt-0 md:ml-4">
+    <div className="flex items-center">
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="px-3 py-1 rounded-md border hover:bg-black/5">登录</button>
+          <button className="h-9 px-3 text-sm rounded-md border hover:bg-black/5">
+            登录
+          </button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
