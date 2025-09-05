63edbc3d3dd358031d0f830742574ba6b45a725f
SoMic520 <jhzhou520@gmail.com>
2025-09-05 17:40:10 +0800

    Update Header.js
    增加登录按钮

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -15,11 +15,14 @@ import SideBar from './SideBar'
 import SideBarDrawer from './SideBarDrawer'
 import TagGroups from './TagGroups'
 
+// 新增导入 Clerk 用户按钮
+import { UserButton } from '@clerk/nextjs'
+
 let windowTop = 0
 
 /**
  * 顶部导航
- * @param {*} param0
+ * @param {*} props
  * @returns
  */
 const Header = props => {
@@ -28,18 +31,18 @@ const Header = props => {
   const { locale } = useGlobal()
   const router = useRouter()
   const [isOpen, changeShow] = useState(false)
+
   const showSearchButton = siteConfig('HEXO_MENU_SEARCH', false, CONFIG)
   const showRandomButton = siteConfig('HEXO_MENU_RANDOM', false, CONFIG)
 
   const toggleMenuOpen = () => {
     changeShow(!isOpen)
   }
-
   const toggleSideBarClose = () => {
     changeShow(false)
   }
 
-  // 监听滚动
+  // 监听滚动，控制导航样式
   useEffect(() => {
     window.addEventListener('scroll', topNavStyleHandler)
     router.events.on('routeChangeComplete', topNavStyleHandler)
@@ -51,18 +54,13 @@ const Header = props => {
   }, [])
 
   const throttleMs = 200
-
   const topNavStyleHandler = useCallback(
     throttle(() => {
       const scrollS = window.scrollY
       const nav = document.querySelector('#sticky-nav')
-      // 首页和文章页会有头图
       const header = document.querySelector('#header')
-      // 导航栏和头图是否重叠
       const scrollInHeader =
-        header && (scrollS < 10 || scrollS < header?.clientHeight - 50) // 透明导航条的条件
-
-      // const textWhite = header && scrollInHeader
+        header && (scrollS < 10 || scrollS < header?.clientHeight - 50)
 
       if (scrollInHeader) {
         nav && nav.classList.replace('bg-white', 'bg-none')
@@ -82,7 +80,7 @@ const Header = props => {
         nav && nav.classList.replace('text-white', 'text-black')
       }
 
-      // 导航栏不在头图里，且页面向下滚动一定程度 隐藏导航栏
+      // 控制导航条显隐
       const showNav =
         scrollS <= windowTop ||
         scrollS < 5 ||
@@ -97,6 +95,7 @@ const Header = props => {
     }, throttleMs)
   )
 
+  // 搜索抽屉内容，省略不相关 JSX
   const searchDrawerSlot = (
     <>
       {categories && (
@@ -107,16 +106,13 @@ const Header = props => {
               {locale.COMMON.CATEGORY}
             </div>
             <SmartLink
-              href={'/category'}
+              href='/category'
               passHref
               className='mb-3 text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white hover:underline cursor-pointer'>
               {locale.COMMON.MORE} <i className='fas fa-angle-double-right' />
             </SmartLink>
           </div>
-          <CategoryGroup
-            currentCategory={currentCategory}
-            categories={categories}
-          />
+          <CategoryGroup currentCategory={currentCategory} categories={categories} />
         </section>
       )}
 
@@ -128,9 +124,9 @@ const Header = props => {
               {locale.COMMON.TAGS}
             </div>
             <SmartLink
-              href={'/tag'}
+              href='/tag'
               passHref
-              className='text-gray-400 hover:text-black  dark:hover:text-white hover:underline cursor-pointer'>
+              className='text-gray-400 hover:text-black dark:hover:text-white hover:underline cursor-pointer'>
               {locale.COMMON.MORE} <i className='fas fa-angle-double-right' />
             </SmartLink>
           </div>
@@ -144,43 +140,46 @@ const Header = props => {
 
   return (
     <div id='top-nav' className='z-40'>
+      {/* 搜索抽屉 */}
       <SearchDrawer cRef={searchDrawer} slot={searchDrawerSlot} />
 
       {/* 导航栏 */}
       <div
         id='sticky-nav'
         style={{ backdropFilter: 'blur(3px)' }}
-        className={
-          'top-0 duration-300 transition-all  shadow-none fixed bg-none dark:bg-hexo-black-gray dark:text-gray-200 text-black w-full z-20 transform border-transparent dark:border-transparent'
-        }>
+        className='top-0 duration-300 transition-all shadow-none fixed bg-none dark:bg-hexo-black-gray dark:text-gray-200 text-black w-full z-20 transform border-transparent dark:border-transparent'>
         <div className='w-full flex justify-between items-center px-4 py-2'>
+          {/* 左侧 Logo */}
           <div className='flex'>
             <Logo {...props} />
           </div>
 
-          {/* 右侧功能 */}
-          <div className='mr-1 flex justify-end items-center '>
+          {/* 右侧功能区 */}
+          <div className='mr-1 flex justify-end items-center'>
+            {/* 桌面端菜单 */}
             <div className='hidden lg:flex'>
-              {' '}
               <MenuListTop {...props} />
             </div>
+
+            {/* 新增用户按钮：显示个人头像、个人中心/退出登录等 */}
+            <UserButton afterSignOutUrl='/' />
+
+            {/* 移动端菜单开关 */}
             <div
               onClick={toggleMenuOpen}
               className='w-8 justify-center items-center h-8 cursor-pointer flex lg:hidden'>
-              {isOpen ? (
-                <i className='fas fa-times' />
-              ) : (
-                <i className='fas fa-bars' />
-              )}
+              {isOpen ? <i className='fas fa-times' /> : <i className='fas fa-bars' />}
             </div>
-            {showSearchButton && <SearchButton />}
+
+            {/* 搜索按钮和随机文章按钮根据配置显示 */}
+            {showSearchButton && <SearchButton {...props} />}
             {showRandomButton && <ButtonRandomPost {...props} />}
           </div>
         </div>
       </div>
 
       {/* 折叠侧边栏 */}
-      <SideBarDrawer isOpen={isOpen} onClose={toggleSideBarClose}>
+      <SideBarDrawer onClose={toggleSideBarClose}>
         <SideBar {...props} />
       </SideBarDrawer>
     </div>
