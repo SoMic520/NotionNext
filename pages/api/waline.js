baa93a1e4614b20db79054d57222fda75c1ea4b6
SoMic520 <jhzhou520@gmail.com>
2025-09-06 22:31:12 +0800

    Update waline.js

--- pages/api/waline.js
+++ pages/api/waline.js
@@ -1,6 +1,4 @@
 // pages/api/waline.js
-import Waline from '@waline/vercel'
+const Waline = require('@waline/vercel')
 
-export 默认 Waline({
-  // 先留空，跑通再说；要定制可以加 locales / plugins
-})
+module。exports = Waline({})
