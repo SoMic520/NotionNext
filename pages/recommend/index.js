8cc5970beaaf29a147441d674b9264542246af73
SoMic520 <jhzhou520@gmail.com>
2025-09-11 15:01:47 +0800

    Create index.js

---
+++ pages/recommend/index.js
@@ -0,0 +1,25 @@
+import BLOG from '@/blog.config'
+import { siteConfig } from '@/lib/config'
+import { getGlobalData } from '@/lib/db/getSiteData'
+import { DynamicLayout } from '@/themes/theme'
+
+export default function Recommend (props) {
+  return (
+    <DynamicLayout
+      theme={siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)}
+      layoutName='LayoutRecommend'
+      {...props}
+    />
+  )
+}
+
+export async function getStaticProps({ locale }) {
+  const props = await getGlobalData({ from: 'Recommend', locale })
+  delete props.allPages
+  return {
+    props,
+    revalidate: process.env.EXPORT
+      ? undefined
+      : siteConfig('NEXT_REVALIDATE_SECOND', BLOG.NEXT_REVALIDATE_SECOND, props.NOTION_CONFIG)
+  }
+}
