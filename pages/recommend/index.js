06df820fe3f4d1e6035fb1ae5d40158196e81b93
SoMic520 <jhzhou520@gmail.com>
2025-09-11 14:00:42 +0800

    Create index.js

---
+++ pages/recommend/index.js
@@ -0,0 +1,25 @@
+import BLOG from '@/blog.config'
+import { siteConfig } from '@/lib/config'
+import { getGlobalData } from '@/lib/db/getSiteData'
+import { DynamicLayout } from '@/themes/theme'
+
+export default function Recommend (props) {
+  return (
+    <DynamicLayout
+      theme={siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)}
+      layoutName='LayoutRecommend'  // 指定我们自定义的布局名
+      {...props}
+    />
+  )
+}
+
+export async function getStaticProps({ locale }) {
+  const props = await getGlobalData({ from: 'Recommend', locale })
+  delete props.allPages
+  return {
+    props,
+    revalidate: process.env.EXPORT
+      ? undefined
+      : siteConfig('NEXT_REVALIDATE_SECOND', BLOG.NEXT_REVALIDATE_SECOND, props.NOTION_CONFIG)
+  }
+}
