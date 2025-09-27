259081aa7cbd7e585afa377177e633b6104776a9
SoMic520 <jhzhou520@gmail.com>
2025-09-27 14:48:06 +0800

    Update next-sitemap.config.js
    sitemapSize: 7000,  // 修复语法错误：添加逗号

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,8 +3,8 @@ const BLOG = require('./blog.config')
 /**
  * 通常没啥用，sitemap交给 /pages/sitemap.xml.js 动态生成
  */
-module。exports = {
-  siteUrl: BLOG.LINK，  // 确保BLOG.LINK是正确的
+module.exports = {  // 将中文句号改为英文句号
+  siteUrl: BLOG.LINK,  // 确保BLOG.LINK是正确的
   changefreq: 'daily',
   priority: 0.7,
   generateRobotsTxt: true,
